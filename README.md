ab18634adfd240936f999cb80450af2ef734519c
bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-10-10 11:42:37 -0300

    readme com nome e rm dos integrantes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-#Checkpoint 5
+# Checkpoint 5 - Mobile
+
+## Lista de Compras
+
+### Bruno Ciccio RM:99097
+### Isabelle Corsi RM:97751
+### José Duarte RM:99488
+### Thalita Alencar RM:99292
---
+++ src/components/CreateProduto.js
@@ -0,0 +1,39 @@
+const CreateProduto = () => {
+    const { addProduto } = useFinance();
+    const [description, setDescription] = useState('');
+    const [amount, setAmount] = useState('');
+    const [type, setType] = useState('income');
+  
+    const handleSubmit = () => {
+      if (!description || !amount || isNaN(amount)) {
+        Alert.alert('Erro', 'Por favor, insira valores válidos.');
+        return;
+      }
+      addTransaction(description, amount, type);
+      setDescription('')
+      setAmount('');
+      Alert.alert('Sucesso', 'Transação adicionada!');
+    };
+  
+    return (
+      <View>
+        <TextInput
+          placeholder="Descrição"
+          value={description}
+          onChangeText={setDescription}
+          style={{ borderBottomWidth: 1, marginBottom: 10 }}
+        />
+        <TextInput
+          placeholder="Valor"
+          value={amount}
+          keyboardType="numeric"
+          onChangeText={setAmount}
+          style={{ borderBottomWidth: 1, marginBottom: 10 }}
+        />
+        <Button title="Adicionar Receita" onPress={() => { setType('income'); handleSubmit(); }} />
+        <Button title="Adicionar Despesa" onPress={() => { setType('expense'); handleSubmit(); }} />
+      </View>
+    );
+  };
+  
+  export default FinanceForm;
